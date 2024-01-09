70e78aa36dc66755dff1b690b7569cb2e4ccf3ff
Slava <slavik3001@rambler.ru>
2024-01-09 16:05:26 +0300

    Сделана первоначальная верстка страницы

--- authorization/authorization_page.py
+++ authorization/authorization_page.py
@@ -9,3 +9,34 @@ class AuthorizationPage(Tk):
 
         self.title("Авторизация")
         self.geometry("600x400")
+
+        self.submit_error_text = StringVar()
+
+        users_data_frame = Frame(borderwidth=1, relief=SOLID)
+        users_data_frame.pack(pady=(20, 0))
+
+        users_data_frame_name = ttk.Label(users_data_frame, text="Авторизация пользователя")
+        users_data_frame_name.pack(pady=(20, 30), padx=20)
+
+        email_name_lbl = ttk.Label(users_data_frame, text="Введите email:")
+        email_name_lbl.pack()
+        email_ent = ttk.Entry(users_data_frame)
+        email_ent.pack()
+
+        password_name_lbl = ttk.Label(users_data_frame, text="Введите пароль:")
+        password_name_lbl.pack(pady=(10, 0))
+        password_ent = ttk.Entry(users_data_frame)
+        password_ent.pack(pady=(0, 20))
+
+        submit_btn = Button(text="Войти")
+        submit_btn.pack(pady=(30, 0))
+        submit_btn_error_text_lbl = ttk.Label(textvariable=self.submit_error_text)
+        submit_btn_error_text_lbl.pack()
+
+        registration_btn = Button(text="Зарегистрироваться")
+        registration_btn.pack(pady=(20, 0))
+
+
+if __name__ == "__main__":
+    authorization_page = AuthorizationPage()
+    authorization_page.mainloop()
