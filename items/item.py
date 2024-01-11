9944f74ecca596aec6be1d211317099806e7a7fb
Slava <slavik3001@rambler.ru>
2024-01-11 22:23:14 +0300

    Добавлено абстракное свойство "picture"

--- items/item.py
+++ items/item.py
@@ -1,11 +1,12 @@
 from random import choice
-from abc import ABC, abstractmethod
+from abc import ABC
 
 
 class Item(ABC):
 
     def __init__(self):
-        self.items = []
+        self.picture = None
+        self.items: list = []
 
     def get(self):
         item = choice(self.items)
