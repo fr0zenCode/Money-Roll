f7e567f4d20be6816eb59fdf6dc7a09324854ca5
Slava <slavik3001@rambler.ru>
2024-01-16 22:21:58 +0300

    Изменение логики определения фрейма контента, убран закомментированный код

--- user_package/account_page.py
+++ user_package/account_page.py
@@ -25,8 +25,10 @@ class AccountPage(Tk):
         self.minsize(int(451 * self.percentage_width_from_full_hd), int(451 * self.percentage_height_from_full_hd))
 
         # fonts
+
         self.font_for_lbl = self.create_font_for_lbl(
             10 * (self.percentage_width_from_full_hd + self.percentage_height_from_full_hd) / 2)
+
         self.font_for_ent = self.font_for_lbl
         self.font_for_combobox = self.font_for_lbl
 
@@ -36,10 +38,10 @@ class AccountPage(Tk):
         ###############################################################################################################
         content_frame = Frame(
             background="green",
-            width=int(600 * self.percentage_width_from_full_hd),
-            height=int(400 * self.percentage_height_from_full_hd)
+            # width=int(800 * self.percentage_width_from_full_hd),
+            # height=int(500 * self.percentage_height_from_full_hd)
         )
-        content_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
+        content_frame.place(relx=0.5, rely=0.5, anchor=CENTER, relwidth=0.5, relheight=0.5)
         content_frame.pack_propagate(False)
         ###############################################################################################################
 
@@ -59,12 +61,25 @@ class AccountPage(Tk):
 
 
 
-
         photo_frame = Frame(avatar_frame, background="blue")
         photo_frame.pack(expand=True, fill=BOTH)
         photo_frame.pack_propagate(False)
-        photo_lbl = ttk.Label(photo_frame, text="Photo")
-        photo_lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+        self.photo_variant_string = StringVar()
+        self.photo_variant_string.set("../img/log_in_logo.png")
+
+        self.img = Image.open(self.photo_variant_string.get())
+        self.resized_image = self.img.resize((
+            int(200 * self.percentage_width_from_full_hd),
+            int(300 * self.percentage_height_from_full_hd))
+        )
+        self.new_image = ImageTk.PhotoImage(self.resized_image)
+
+        self.label_avatar = ttk.Label(photo_frame, image=self.new_image)
+        self.label_avatar.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+
+
 
         photo_selector_frame = Frame(avatar_frame, background="pink")
         photo_selector_frame.pack(fill=BOTH)
@@ -80,6 +95,8 @@ class AccountPage(Tk):
         )
         photo_selector_combobox["values"] = photo_variants
         photo_selector_combobox.pack(pady=(20 * self.percentage_width_from_full_hd, 60 * self.percentage_height_from_full_hd))
+
+
 
 
 
@@ -115,55 +132,8 @@ class AccountPage(Tk):
         ###############################################################################################################
         ###############################################################################################################
 
-        #     # player's photo frame
-        #     ###############################################################################################################
-        #     photo_frame = Frame(
-        #         content_frame,
-        #         background="blue",
-        #         width=int(130 * self.percentage_width_from_full_hd),
-        #         height=int(160 * self.percentage_height_from_full_hd)
-        #     )
-        #     photo_frame.place(relx=0.04, rely=0.05, anchor=NW)
-        #
-        #     # string var for photo selector
-        #     self.photo_variant_string = StringVar()
-        #     self.photo_variant_string.set("../img/log_in_logo.png")
-        #
-        #     self.img = Image.open(self.photo_variant_string.get())
-        #     self.resized_image = self.img.resize((
-        #         int(110 * self.percentage_width_from_full_hd),
-        #         int(150 * self.percentage_height_from_full_hd))
-        #     )
-        #     self.new_image = ImageTk.PhotoImage(self.resized_image)
-        #
-        #     self.label_avatar = ttk.Label(photo_frame, image=self.new_image)
-        #     self.label_avatar.place(relx=0.5, rely=0.5, anchor=CENTER)
-        #     ###############################################################################################################
-        #
-        #     # photo selector frame
-        #     ###############################################################################################################
-        #     photo_selector_frame = Frame(
-        #         content_frame,
-        #         background="yellow",
-        #         width=int(120 * self.percentage_width_from_full_hd),
-        #         height=int(50 * self.percentage_height_from_full_hd)
-        #     )
-        #     photo_selector_frame.place(relx=0.19, rely=0.7, anchor=CENTER)
-        #
-        #     self.photo_variants = ("<Выберите аватар>", "Лудоман", "Озорник", "Плохой парень")
-        #     self.combobox_variable = StringVar()
-        #     self.combobox_variable.set(self.photo_variants[0])
-        #     self.photo_selector_combobox = ttk.Combobox(photo_selector_frame, textvariable=self.combobox_variable)
-        #     self.photo_selector_combobox["values"] = self.photo_variants
-        #     self.photo_selector_combobox.place(relx=0.5, rely=0.1, anchor=CENTER)
-        #
-        #     self.photo_selector_combobox.bind(
-        #         "<<ComboboxSelected>>",
-        #         lambda event: self.photo_selector_action(self.combobox_variable.get())
-        #     )
-        #     ###############################################################################################################
-        #
-        #
+
+
 
     @staticmethod
     def create_font_for_lbl(font_size):
