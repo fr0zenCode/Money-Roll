e24a9625a5677f10ad4d8d1cc8374fc8c9c4c2b5
Slava <slavik3001@rambler.ru>
2024-01-20 12:05:20 +0300

    Убрана лишняя логика

--- main_window_package/game.py
+++ main_window_package/game.py
@@ -1,19 +1,16 @@
 from random import random
 
-from items.golden_item import GoldenItem
-from items.red_item import RedItem
-from items.purple_item import PurpleItem
-from items.dark_purple_item import DarkPurpleItem
-from items.blue_item import BlueItem
+import items.blue_item as blue_item
+import items.dark_purple_item as dark_purple_item
+import items.purple_item as purple_item
+import items.red_item as red_item
+import items.golden_item as golden_item
 
 
 class Game:
 
     def __init__(self, player):
         self.player = player
-        self.total = 0
-        self.total_win_text = ""
-        self.current_win_text = ""
 
     @staticmethod
     def _make_drop(item_type_object):
@@ -26,26 +23,21 @@ class Game:
         chance = random() * 100
 
         if chance <= 0.05:
-            return self._make_drop(GoldenItem)
+            return self._make_drop(golden_item.GoldenItem)
         if 0.05 < chance <= 0.20:
-            return self._make_drop(RedItem)
+            return self._make_drop(red_item.RedItem)
         if 0.20 < chance <= 2:
-            return self._make_drop(PurpleItem)
+            return self._make_drop(purple_item.PurpleItem)
         if 2 < chance <= 10:
-            return self._make_drop(DarkPurpleItem)
+            return self._make_drop(dark_purple_item.DarkPurpleItem)
 
-        return self._make_drop(BlueItem)
+        return self._make_drop(blue_item.BlueItem)
 
     def get_prize(self):
-
         item = self._choose_item()
         self.player.take_money_for_game()
-        self.total += item[0]
-        self.current_win_text = f"Поздравляем! Вы выиграли {item} рублей."
-
         return item
 
 
 if __name__ == '__main__':
-    game = Game("player")
-    game.run()
+    pass
