da90cdb1807453435affb4b49382fa6481b06c73
Slava <slavik3001@rambler.ru>
2024-01-20 11:58:19 +0300

    Добавлено взаимодействие с БД, изменен дизайн.

--- user_package/top_up_the_balance_page.py
+++ user_package/top_up_the_balance_page.py
@@ -1,32 +1,35 @@
 from tkinter import *
-from tkinter import ttk
-import tkinter.font as tk_font
-
 import customtkinter as ctk
 
+import connection
+from settings import (background_color, buttons_background_color, buttons_hover_background_color, areas_background,
+                      user as db_user, password as db_password, database as db_name)
+
 import user_package.user as user
-import user_package.bank_card_frame as bank_card_frame
-import authorization.functions as functions
 import main_window_package.main_window as main_window
 
 
-class TopUpTheBalancePage(Tk):
+class TopUpTheBalancePage(ctk.CTk):
 
     def __init__(self, player: user.User):
         super().__init__()
 
-        self.user = player
+        self.player = player
 
         # window settings
         self.state("zoomed")
-        self.background_color = "pink"
 
-        self.minsize(1000, 600)
+        self.screen_width = self.winfo_screenwidth()
+        self.screen_height = self.winfo_screenheight()
 
-        self.configure(background=self.background_color)
+        self.minsize(int(self.screen_width / 1.7), int(self.screen_height / 1.7))
 
-        self.content_frame = ctk.CTkFrame(self, bg_color=self.background_color)
+        self.configure(fg_color=background_color)
+
+        self.content_frame = ctk.CTkFrame(self, fg_color=background_color)
         self.content_frame.place(relx=0.5, rely=0.5, anchor=CENTER, relwidth=1, relheight=1)
+
+        self.connection = connection.Connection()
 
         # back button
         self.back_btn = self.create_back_button(self.content_frame)
@@ -36,15 +39,16 @@ class TopUpTheBalancePage(Tk):
 
         # card main frame
         ###############################################################################################################
-        self.card_background_color = "#B4B4B4"
-        self.card_entries_background = "#5F5F5F"
+        self.card_background_color = "#5D6086"
+        self.card_entries_background = "#344B6A"
+        self.card_labels_text_color = "#44D7E2"
 
         card_frame = ctk.CTkFrame(
             self.content_frame,
             corner_radius=30,
             fg_color=self.card_background_color,
         )
-        card_frame.place(relx=0.5, rely=0.1, anchor=N, relwidth=0.55, relheight=0.29)
+        card_frame.place(relx=0.5, rely=0.1, anchor=N, relwidth=0.3, relheight=0.29)
         ###############################################################################################################
 
         # card number frame
@@ -61,8 +65,7 @@ class TopUpTheBalancePage(Tk):
         card_number_lbl = ctk.CTkLabel(
             card_number_frame,
             text="Номер банковской карты",
-            fg_color=self.card_background_color,
-            text_color=self.card_entries_background,
+            text_color=self.card_labels_text_color,
             font=self.main_font
         )
         card_number_lbl.place(relx=0.5, rely=0.1, anchor=N, relwidth=0.9, relheight=0.3)
@@ -84,28 +87,35 @@ class TopUpTheBalancePage(Tk):
             card_frame,
             fg_color=self.card_background_color
         )
-        card_date_frame.place(relx=0.1, rely=0.8, anchor=W, relwidth=0.32, relheight=0.22)
+        card_date_frame.place(relx=0.1, rely=0.8, anchor=W, relwidth=0.4, relheight=0.3)
 
-        self.card_date_validater = (self.register(self.card_date_validater_logic), "%P", "%d")
+        self.card_date_validater = (self.register(self.card_date_validater_logic), "%P")
 
-        card_date_lbl = ctk.CTkLabel(
+        card_date_mouth_lbl = ctk.CTkLabel(
             card_date_frame,
-            text="ММ/ГГ:",
-            fg_color=self.card_background_color,
-            text_color=self.card_entries_background,
-            font=self.main_font
+            text="ММ:"
         )
-        card_date_lbl.place(relx=0.1, rely=0.5, anchor=W, relwidth=0.4)
-
-        self.card_date_ent = ctk.CTkEntry(
+        card_date_mouth_lbl.place(relx=0.1, rely=0.5, anchor=W, relwidth=0.2, relheight=0.5)
+        self.card_date_mouth_ent = ctk.CTkEntry(
             card_date_frame,
-            fg_color=self.card_entries_background,
+            justify="center",
             validate="key",
-            validatecommand=self.card_date_validater,
-            justify=CENTER,
-            font=self.main_font
+            validatecommand=self.card_date_validater
         )
-        self.card_date_ent.place(relx=0.9, rely=0.5, anchor=E, relwidth=0.4)
+        self.card_date_mouth_ent.place(relx=0.3, rely=0.5, anchor=W, relwidth=0.2, relheight=0.5)
+
+        card_date_slash_lbl = ctk.CTkLabel(card_date_frame, text="/")
+        card_date_slash_lbl.place(relx=0.5, rely=0.5, anchor=W, relwidth=0.05, relheight=0.5)
+
+        card_date_year_lbl = ctk.CTkLabel(card_date_frame, text="ГГ:")
+        card_date_year_lbl.place(relx=0.55, rely=0.5, anchor=W, relwidth=0.2, relheight=0.5)
+        self.card_date_year_ent = ctk.CTkEntry(
+            card_date_frame,
+            justify="center",
+            validate="key",
+            validatecommand=self.card_date_validater
+        )
+        self.card_date_year_ent.place(relx=0.75, rely=0.5, anchor="w", relwidth=0.2, relheight=0.5)
         ###############################################################################################################
 
         # card CVV frame
@@ -114,7 +124,7 @@ class TopUpTheBalancePage(Tk):
             card_frame,
             fg_color=self.card_background_color
         )
-        card_cvv_frame.place(relx=0.9, rely=0.8, anchor=E, relwidth=0.3, relheight=0.2)
+        card_cvv_frame.place(relx=0.9, rely=0.8, anchor=E, relwidth=0.35, relheight=0.3)
 
         self.card_cvv_validater = (self.register(self.cvv_validater_logic), "%P")
 
@@ -122,7 +132,7 @@ class TopUpTheBalancePage(Tk):
             card_cvv_frame,
             text="CVV:",
             bg_color=self.card_background_color,
-            text_color=self.card_entries_background,
+            text_color=self.card_labels_text_color,
             font=self.main_font
         )
         card_cvv_lbl.place(relx=0.1, rely=0.5, anchor=W, relwidth=0.4)
@@ -139,16 +149,10 @@ class TopUpTheBalancePage(Tk):
         ###############################################################################################################
 
         # email for recipe frame
-        # last frame:
-        # relative x=0.5,
-        # relative y=0.1,
-        # anchor=N,
-        # relative width=0.55,
-        # relative height=0.29
         ###############################################################################################################
         email_for_recipe_frame = ctk.CTkFrame(
             self.content_frame,
-            bg_color=self.background_color
+            fg_color=background_color
         )
         email_for_recipe_frame.place(relx=0.5, rely=0.45, anchor=N, relwidth=0.55, relheight=0.07)
 
@@ -162,21 +166,17 @@ class TopUpTheBalancePage(Tk):
         self.email_for_recipe_ent = ctk.CTkEntry(
             email_for_recipe_frame,
             justify=CENTER,
-            font=self.main_font
+            font=self.main_font,
+            fg_color=areas_background
         )
         self.email_for_recipe_ent.place(relx=0.5, rely=0.75, anchor=CENTER, relwidth=0.8, relheight=0.5)
         ###############################################################################################################
 
         # sum for make a payment
-        # last frame:
-        # relative x=0.5
-        # relative y=0.45
-        # anchor=N
-        # relative width=0.55
-        # relative height=0.07
         ###############################################################################################################
         sum_frame = ctk.CTkFrame(
-            self.content_frame
+            self.content_frame,
+            fg_color=background_color
         )
         sum_frame.place(relx=0.5, rely=0.6, anchor=N, relwidth=0.25, relheight=0.1)
 
@@ -194,28 +194,25 @@ class TopUpTheBalancePage(Tk):
             justify=CENTER,
             validate="key",
             validatecommand=self.sum_validate,
-            font=self.font_for_important_objects
+            font=self.font_for_important_objects,
+            fg_color=areas_background
         )
         self.sum_ent.place(relx=0.5, rely=0.75, anchor=CENTER, relwidth=0.9, relheight=0.5)
         ###############################################################################################################
 
         # submit button frame
-        # last frame:
-        # relative x=0.5
-        # relative y=0.6
-        # anchor=N
-        # relative width=0.25
-        # relative height=0.1
         ###############################################################################################################
-        submit_button_frame = ctk.CTkFrame(
-            self.content_frame
-        )
+        submit_button_frame = ctk.CTkFrame(self.content_frame, fg_color=background_color)
         submit_button_frame.place(relx=0.5, rely=0.8, anchor=CENTER, relwidth=0.25, relheight=0.1)
 
         submit_btn = ctk.CTkButton(
             submit_button_frame,
             text="Оплатить",
             cursor="hand2",
+            border_color="white",
+            border_width=2,
+            fg_color=buttons_background_color,
+            hover_color=buttons_hover_background_color,
             command=self.submit_btn_action
         )
         submit_btn.place(relx=0.5, rely=0.5, anchor=CENTER, relwidth=0.85, relheight=0.7)
@@ -227,9 +224,13 @@ class TopUpTheBalancePage(Tk):
             master,
             text="Назад",
             cursor="hand2",
-            command=self.back_btn_action
+            command=self.back_btn_action,
+            fg_color=buttons_background_color,
+            hover_color=buttons_hover_background_color,
+            border_width=1,
+            border_color="grey"
         )
-        back_btn.place(relx=0.01, rely=0.01, anchor=NW, relwidth=0.12, relheight=0.035)
+        back_btn.place(relx=0.01, rely=0.01, anchor=NW, relwidth=0.07, relheight=0.035)
         return back_btn
 
     @staticmethod
@@ -244,56 +245,24 @@ class TopUpTheBalancePage(Tk):
                 return True
             return False
 
-    def card_date_validater_logic(self, card_date, action_type):
+    @staticmethod
+    def card_date_validater_logic(card_date):
 
-        if action_type == "0":
-            if len(card_date) >= 3:
-                if "/" not in card_date:
-                    return False
-                else:
-                    if card_date[2] != "/":
-                        return False
-                    return True
+        if card_date:
 
-        if " " in card_date:
-            return False
-        if 6 > len(card_date) >= 4:
-            try:
-                int(card_date[3::])
-                return True
-            except ValueError:
-                return False
-        if len(card_date) > 5:
-            return False
-        if len(card_date) >= 2 and action_type != "0":
             if len(card_date) > 2:
-                if card_date[2] == "/":
-                    return True
-                else:
-                    if card_date == " ":
-                        return False
-                    try:
-                        int(card_date)
-                    except ValueError:
-                        return False
-                    self.card_date_ent.delete(0, 1)
-                    self.card_date_ent.insert(0, f"{card_date[:2]}/{card_date[2:]}")
-                    return False
+                return False
+            if " " in card_date:
+                return False
+
             try:
                 int(card_date)
-                self.card_date_ent.delete(0, 5)
-                self.card_date_ent.insert(0, f"{card_date}/")
+                return True
             except ValueError:
                 return False
-        try:
-            int(card_date[:2])
-            return True
-        except ValueError:
-            if len(card_date) == 0:
-                return True
-            return False
 
-    # TODO: test
+        return True
+
     @staticmethod
     def validate_card_number_logic(card_number):
         if len(card_number) > 16:
@@ -319,25 +288,42 @@ class TopUpTheBalancePage(Tk):
             return False
 
     def back_btn_action(self):
-        player = self.user
+        player = self.player
         self.destroy()
         new_window = main_window.MainWindow(player)
         new_window.mainloop()
 
     def submit_btn_action(self):
-        card_number = self.bank_card_frame.card_number_ent.get()
-        card_date = self.bank_card_frame.card_date_ent.get()
-        card_cvv = self.bank_card_frame.card_cvv_ent.get()
+        card_number = self.card_number_ent.get()
+
+        card_date_mouth = self.card_date_mouth_ent.get()
+        card_date_year = self.card_date_year_ent.get()
+
+        card_cvv = self.card_cvv_ent.get()
         email_for_recipe = self.email_for_recipe_ent.get()
         sum_for_increase = int(self.sum_ent.get())
 
-        client = self.user
+        client = self.player
 
-        if len(card_number) == 16 and len(card_date) == 5 and len(card_cvv) == 3 and email_for_recipe and int(
-                sum_for_increase) > 0:
+        if (len(card_number) == 16 and len(card_date_mouth) == 2 and len(card_date_year) == 2 and len(card_cvv) == 3
+                and email_for_recipe and int(sum_for_increase) > 0):
             client.increase_balance(sum_for_increase)
+
+            if self.connection.get_connection() is None:
+                self.connection.make_connection(user=db_user, password=db_password, database=db_name)
+
+            self.connection.get_cursor().execute(f"""
+            UPDATE "authorization-data" 
+            SET balance = '{self.player.get_balance()}' 
+            WHERE login = '{self.player.login}' 
+            AND password = '{self.player.get_password()}' 
+            AND email = '{self.player.user_email}';"""
+                                                 )
+
+            self.connection.get_connection().commit()
+
             self.destroy()
-            new_window = main_window.MainWindow(self.user)
+            new_window = main_window.MainWindow(self.player)
             new_window.mainloop()
 
         else:
